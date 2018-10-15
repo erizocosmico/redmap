54900cdf1a10045164d3688c6ff2caf4361820e7
Miguel Molina <miguel@erizocosmi.co>
2018-10-15 15:53:25 +0200

    ci: disable -race
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

--- go.test.sh
+++ go.test.sh
@@ -6,7 +6,7 @@ echo "" > coverage.txt
 CGO_ENABLED=0 go build -buildmode=plugin -o ./_testdata/job ./_testdata/job.go
 CGO_ENABLED=0 go build -buildmode=plugin -o ./_testdata/reduce_error ./_testdata/reduce_error.go
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+    go test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
